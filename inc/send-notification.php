5a85264147f6d7efa3a197e825b21e35af4e6e9d
Yifei He <yifei@hesyifei.com>
2019-03-22 13:36:41 -0700

    make sure notifications with >100 receivers could be sent

--- inc/send-notification.php
+++ inc/send-notification.php
@@ -48,29 +48,46 @@ function tsd_send_expo_push_notification( $receiver_pn_users_ids, $title, $body,
 		$all_messages[] = $each_message;
 	}
 
-	// Ref: https://docs.expo.io/versions/latest/guides/push-notifications/#http2-api
-	// TODO: "an array of up to 100 messages" - need divide 100
-	$response = wp_safe_remote_post( "https://exp.host/--/api/v2/push/send", [
-		'method' => 'POST',
-		'timeout' => 15,
-		'httpversion' => '2.0',
-		'headers' => [ "content-type" => "application/json" ],
-		'body' => json_encode( $all_messages ),
-	] );
-
-	if ( is_wp_error( $response ) ) {
-		$error_message = $response->get_error_message();
-		tsd_pn_set_admin_notice( 'fail', "Response: \n" . json_encode( $error_message ) . "\nYour message: \n" . json_encode( $message_body ) );
-		return false;
+	// Because Expo HTTP request can only consist of "an array of up to 100 messages".
+	$all_messages_chucked = array_chunk( $all_messages, 99 );
+	$responses = [];
+	foreach ( $all_messages_chucked as $each_messages_chucked ) {
+		// Ref: https://docs.expo.io/versions/latest/guides/push-notifications/#http2-api
+		$responses[] = wp_safe_remote_post( "https://exp.host/--/api/v2/push/send", [
+			'method' => 'POST',
+			'timeout' => 15,
+			'httpversion' => '2.0',
+			'headers' => [ "content-type" => "application/json" ],
+			'body' => json_encode( $all_messages ),
+		] );
 	}
 
-	$decoded_body = json_decode( $response[ "body" ], true );
-	if ( $response[ "response" ][ "code" ] != 200 ) {
-		tsd_pn_set_admin_notice( 'fail', "Response: \n" . json_encode( $decoded_body ) . "\nYour message: \n" . json_encode( $message_body ) );
-		return false;
+
+	$all_responses_success = true;
+	$admin_notice_message = "";
+	foreach ( $responses as $response ) {
+		if ( is_wp_error( $response ) ) {
+			$all_responses_success = false;
+			$error_message = $response->get_error_message();
+			$admin_notice_message .= json_encode( $error_message ) . "\n";
+			continue;
+		}
+
+		$decoded_body = json_decode( $response[ "body" ], true );
+		if ( $response[ "response" ][ "code" ] != 200 ) {
+			$all_responses_success = false;
+			$admin_notice_message .= json_encode( $decoded_body ) . "\n";
+			continue;
+		}
+
+		$admin_notice_message .= json_encode( $decoded_body ) . "\n";
 	}
 
-	tsd_pn_set_admin_notice( 'success', "Response: \n" . json_encode( $decoded_body ) . "\nYour message: \n" . json_encode( $message_body ) );
+	$admin_notice_type = "success";
+	if ( ! $all_responses_success ) {
+		$admin_notice_type = "fail";
+	}
+	tsd_pn_set_admin_notice( $admin_notice_type, "Response: \n" . $admin_notice_message . "\nYour message: \n" . json_encode( $message_body ) );
 
-	return true;
+	return $all_responses_success;
 }
