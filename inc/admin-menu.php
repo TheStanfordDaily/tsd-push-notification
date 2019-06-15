7c8500f33725f33abd5bbbf7f9a851d35a2699c5
Yifei He <yifei@hesyifei.com>
2019-06-15 09:44:45 -0700

    Add a clear debugging log button in the debug log page

--- inc/admin-menu.php
+++ inc/admin-menu.php
@@ -7,16 +7,19 @@ function tsd_pn_plugin_menu() {
 		'tsd-push-notification.php',
 		'tsd_pn_plugin_settings_page'
 	);
-
-	// DEBUG
-	//update_option( "tsd_pn_debug_info", [] );
 }
 add_action( 'admin_menu', 'tsd_pn_plugin_menu' );
 
 function tsd_pn_plugin_settings_page() {
+	if ( isset( $_POST["clear_debug"] ) ) {
+		update_option( "tsd_pn_debug_info", [] );
+	}
 	?>
 <div class="wrap">
 	<h1>TSD Push Notification Debug</h1>
+	<form action="" method="POST">
+		<input type="submit" name="clear_debug" value="Clear Debugging Log" />
+	</form>
 	<pre>
 	<?php var_dump( get_option( "tsd_pn_debug_info" ) ); ?>
 	</pre>
